8fe089775ba275b4704568ac86e470ce07e7de47
Maria Coelho <mariaeduardadeoliveriacoelho1@gmail.com>
2024-06-29 19:53:07 -0300

    Exibindo mensagem de cadastro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -17,9 +18,46 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
     conn = new conectaDAO().connectDB();
-      
-        
+    try{
+     st = conn.prepareStatement("INSERT INTO produtos (nome, valor,status) VALUES (?,?,?)");
+     st.setString(1, produto.getNome());
+     st.setDouble(2, produto.getValor());
+     st.setString(3, produto.getStatus());
+    
+      int status = st.executeUpdate();
+      if (status > 0){
+       JOptionPane.showMessageDialog(null, "Cadastro feito");
+      }
+      else{
+       JOptionPane.showMessageDialog(null, "Erro ao conectar");
+      }
+    } catch(SQLException ex){
+     JOptionPane.showMessageDialog(null,"Erro a o cadastrar produto: " + ex.getMessage());
+    } finally {
+        try{
+         if (conn != null) conn.close();
+         if (st != null) st.close();
+        } catch (SQLException e){
+         JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+        }
+    
+    
+    
+    
     }
+    
+    
+    
+    
+    
+    
+    
+    }
+        
+        
+        
+       
+ 
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
